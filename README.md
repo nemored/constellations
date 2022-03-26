a40cfa55c978a5b27cd46a6578dd5fedecd8a8ad
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-03-26 15:11:07 -0700

    rudimentary logging

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 - logging
 - sign-in/out naming
 - remove dead code
+- update mock data and functions
+- node command line utility
+- systemd service
--- backend/src/graphql/typedef.ts
+++ backend/src/graphql/typedef.ts
@@ -1,5 +1,6 @@
 import { gql } from 'apollo-server-core';
 
+// todo: remove user.email
 export const typeDefs = gql`
   type Bookmark {
     id: Int
